7f92174ccc5b339da289b60490a93387112b9f2c
cn1a2326 <148069975+cn1a2326@users.noreply.github.com>
2023-11-13 09:33:58 -0300

    Create lucas.m

---
+++ lucas.m
@@ -0,0 +1,13 @@
+### *LUCAS MEIRA*🥇
+- bem vindo ao meu perfil
+
+- sou aluno d 1ºA
+
+- no colegio civico militar professor cutodio netto
+
+- tenho 16 anos
+
+- estou me devenvolvendo em linguagem de programação pelo[ALURA].(https://cursos.alura.com.br
+
+- voce pode entrar em contato comigo por lucas.meira.santos11@escola.pr.gov.br
+
